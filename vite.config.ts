9e5160d75ad723d8f5cb93cb13f385d320c3d5b5
Dnovalab <duanjn@foxmail.com>
2026-05-23 21:50:46 +0800

    fix: add resolve.alias for monaco-editor to bypass ESM resolution

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      "monaco-editor": path.resolve(__dirname, "./__mocks__/monaco-editor.ts"),
     },
   },
   build: {
